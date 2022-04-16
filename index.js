0fb87f157161f40f580b47db83da0a4bdf517027
sydney.murray <sydney_i_murray@yahoo.co.uk>
2022-04-16 11:35:33 +0100

    Clear Out

--- index.js
+++
@@ -1,9 +0,0 @@
-let operand = null
-let previousOperator = null
-let displayRunningTotal = false
-const display = document.querySelector("h1")
-document.addEventListener('keydown', keyPressed)
-
-
-
-
